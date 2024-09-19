f65ed9951f0ad3117f4f2abd8d0b8d86705b0b51
Peter Bengtsson <mail@peterbe.com>
2024-09-19 13:49:49 -0400

    Copy buttons (#36)

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,11 @@ export default defineConfig({
         secure: false,
         ws: true,
       },
+      "/cache/": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
