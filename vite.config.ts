b015a158b8daf98da8f47779da86d3b05994c232
Peter Bengtsson <mail@peterbe.com>
2024-10-22 09:12:04 -0400

    Resolve alias for @tabler/icons-react

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // This tip comes from https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
   server: {
     warmup: {
       clientFiles: ["./src/routes.tsx"],
