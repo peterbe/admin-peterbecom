6fb3afd1218a38536d4072bd9820df89e8c49349
Peter Bengtsson <mail@peterbe.com>
2024-10-30 08:30:18 -0400

    navigation search

---
+++ src/components/navigation-search.tsx
@@ -0,0 +1,107 @@
+import {
+  Alert,
+  Autocomplete,
+  type AutocompleteProps,
+  Box,
+  Button,
+  type ComboboxItem,
+  Group,
+  type OptionsFilter,
+  Text,
+} from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation } from "wouter";
+import { blogitemsShowAllQueryKey, fetchShowAllBlogitems } from "./api-utils";
+
+type ShowAllBlogitems = {
+  blogitems: {
+    id: number;
+    oid: string;
+    title: string;
+  }[];
+  count: number;
+};
+
+const escaped = (s: string) => s.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
+
+const optionsFilter: OptionsFilter = ({ options, search }) => {
+  const s = search.toLowerCase().trim();
+  const rex = s ? new RegExp(`\\b${escaped(s)}`, "i") : null;
+
+  const filtered = (options as ComboboxItem[]).filter((option) => {
+    if (!rex) return false;
+    if (!s) return false;
+
+    if (option.label.match(rex) || option.value.match(rex)) {
+      return true;
+    }
+    return false;
+  });
+  return filtered;
+};
+
+const renderAutocompleteOption: AutocompleteProps["renderOption"] = (props) => {
+  const option = props.option as ComboboxItem;
+  return (
+    <Group gap="sm">
+      <div>
+        <Text>{option.label}</Text>
+        <Text size="sm" opacity={0.5}>
+          {option.value}
+        </Text>
+      </div>
+    </Group>
+  );
+};
+
+export function NavigationSearch() {
+  const [, navigate] = useLocation();
+  const { data, isError, error, isPending, refetch } =
+    useQuery<ShowAllBlogitems>({
+      queryKey: blogitemsShowAllQueryKey(),
+      queryFn: fetchShowAllBlogitems,
+      refetchOnWindowFocus: false,
+    });
+
+  const items = !data
+    ? []
+    : data.blogitems.map((blogitem) => ({
+        value: blogitem.oid,
+        label: blogitem.title,
+      }));
+
+  return (
+    <Box>
+      {error && (
+        <Alert color="red" title="Loading error">
+          <Text>Failed to load blogitems: {error.message}</Text>
+          <Button onClick={() => refetch()}>Refresh</Button>
+        </Alert>
+      )}
+      <Autocomplete
+        size="xl"
+        w={500}
+        placeholder="Search titles or OIDs"
+        data={items}
+        filter={optionsFilter}
+        renderOption={renderAutocompleteOption}
+        limit={10}
+        onOptionSubmit={(picked) => {
+          if (picked) {
+            navigate(`/plog/${picked}`);
+          }
+        }}
+        disabled={!data || isError || isPending}
+        rightSection={<IconSearch />}
+        maxDropdownHeight={400}
+        comboboxProps={{
+          shadow: "md",
+          dropdownPadding: 10,
+
+          transitionProps: { transition: "pop", duration: 100 },
+        }}
+      />
+    </Box>
+  );
+}
