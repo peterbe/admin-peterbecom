76b9c816f451983285e3ff32acf2b1f6101f808f
Peter Bengtsson <mail@peterbe.com>
2024-11-07 15:29:07 -0500

    some cleaning up

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,21 +1,14 @@
 import { Box, Button, Group } from "@mantine/core"
 import { useDocumentTitle } from "@mantine/hooks"
-// import { Await, Link, useRouteLoaderData } from "react-router-dom";
 import { Link } from "react-router-dom"
 
 import { useCountUnapprovedComments } from "../hooks/use-count-unapproved-comments"
-// import { Suspense } from "react";
-// import type { RootLoaderData } from "../loaders/root";
 import { SignedIn } from "./signed-in"
 
 export function Home() {
   useDocumentTitle("Home")
 
-  // const { countUnapprovedComments } = useRouteLoaderData(
-  //   "root"
-  // ) as RootLoaderData;
-  const { data, isPending } = useCountUnapprovedComments()
-  console.log("RENDERING HOME COMPONENT", { data: !!data, isPending })
+  const { data } = useCountUnapprovedComments()
 
   return (
     <SignedIn>
@@ -28,17 +21,6 @@ export function Home() {
             Add blogitem
           </Button>
           <CommentsButton count={data?.count} />
-
-          {/* <Suspense fallback={<CommentsButton />}>
-            <Await
-              resolve={countUnapprovedComments}
-              errorElement={<CommentsButton />}
-            >
-              {(countUnapprovedComments) => (
-                <CommentsButton count={countUnapprovedComments.count} />
-              )}
-            </Await>
-          </Suspense> */}
         </Group>
       </Box>
       <Box m={100}>
