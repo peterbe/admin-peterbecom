67245bf6777abe75f60e60520e3deaca8b57a586
Peter Bengtsson <mail@peterbe.com>
2024-11-07 10:54:25 -0500

    moar simple

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,6 @@ import Blogitems from "./components/blogitems";
 import Comments from "./components/comments";
 import ErrorPage from "./components/error-page";
 import { Home } from "./components/home";
-// import { loader as blogitemLoader } from "./loaders/blogitem";
 import { loader as rootLoader } from "./loaders/root";
 import { Root } from "./routes/root";
 
@@ -29,7 +28,6 @@ export const router = createBrowserRouter([
                   {
                     index: true,
                     element: <Blogitem />,
-                    // loader: blogitemLoader,
                   },
                   {
                     path: "open-graph-image",
@@ -43,7 +41,6 @@ export const router = createBrowserRouter([
                 ],
               },
               { index: true, element: <Blogitems /> },
-              // { path: "add", element: <Blogitem />, loader: blogitemLoader },
               { path: "add", element: <Blogitem /> },
               { path: "comments", element: <Comments /> },
             ],
