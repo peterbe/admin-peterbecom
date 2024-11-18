afae047888b456539ada19bf33a9f01ef511f6ea
Peter Bengtsson <mail@peterbe.com>
2024-11-18 13:34:53 -0500

    some knip fixes

--- src/loaders/root.ts
+++ src/loaders/root.ts
@@ -4,10 +4,6 @@ import {
 } from "../components/api-utils"
 import { queryClient } from "../query-client"
 
-export type RootLoaderData = {
-  countUnapprovedComments: number
-}
-
 export async function loader() {
   return {
     countUnapprovedComments: queryClient.fetchQuery({
