bb21cc3e86a82e5acf43d86b06ad24c2faffc284
Peter Bengtsson <mail@peterbe.com>
2025-06-28 14:47:24 -0400

    ability to sort by url

--- src/components/analytics/charts/requests-varying-querystring.tsx
+++ src/components/analytics/charts/requests-varying-querystring.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Table } from "@mantine/core"
+import { Box, Button, Code, Grid, Select, Table, Text } from "@mantine/core"
 import { useClipboard } from "@mantine/hooks"
 import { notifications } from "@mantine/notifications"
 import { useQuery as tsq_useQuery } from "@tanstack/react-query"
@@ -30,15 +30,26 @@ LIMIT ${Number(limit)}
 
 const sqlSearchQuery = (
   path: string,
-  { limit = 200, days = 30, back = 0 } = {},
+  {
+    limit = 200,
+    days = 30,
+    back = 0,
+    statusCode = null,
+  }: {
+    limit?: number
+    days?: number
+    back?: number
+    statusCode?: null | string
+  } = {},
 ) => `
-SELECT url, COUNT(*) AS count
+SELECT url, status_code, COUNT(*) AS count
 FROM requestlog 
 where 
     ${createdRange(days, back)}
     AND request->>'path' = ${escapeString(path)}
-GROUP BY url
-ORDER BY 2 desc 
+    ${statusCode === null ? "" : `AND status_code = ${escapeString(statusCode)}`}
+GROUP BY url, status_code
+ORDER BY 3 desc 
 LIMIT ${Number(limit)}
 `
 
@@ -63,15 +74,16 @@ function Inner() {
 
   const [searchParams] = useSearchParams()
   const searchPath = searchParams.get("searchPath")
+  const searchStatusCode = searchParams.get("searchStatusCode")
   const searchQuery = tsq_useQuery<QueryResult>({
-    queryKey: ["search-query", ID, searchPath],
+    queryKey: ["search-query", ID, searchPath, searchStatusCode],
     queryFn: async () => {
       if (!searchPath) throw new Error("not selected")
       const query = sqlSearchQuery(searchPath, {
         limit: Number(rows),
         days: Number(intervalDays),
+        statusCode: searchStatusCode,
       })
-
       return fetcher(`${apiPrefix}${new URLSearchParams({ query })}`)
     },
     enabled: !!searchPath,
@@ -166,6 +178,9 @@ function URLsTable({ rows }: { rows: QueryResultRow[] }) {
   const clipboard = useClipboard({ timeout: 500 })
   const [clickedUrl, setClickedUrl] = useState("")
 
+  const [searchParams, setSearchParams] = useSearchParams()
+  const searchStatusCode = searchParams.get("searchStatusCode")
+
   useEffect(() => {
     if (clickedUrl) {
       clipboard.copy(clickedUrl)
@@ -186,28 +201,93 @@ function URLsTable({ rows }: { rows: QueryResultRow[] }) {
   }
   const numberFormat = new Intl.NumberFormat("en-US")
 
+  const _statusCodes = new Map<string, number>()
+  const counts = new Map<number, number>()
+  let countAll = 0
+  for (const row of rows) {
+    const s = row.status_code as number
+    counts.set(s, (counts.get(s) || 0) + 1)
+    countAll++
+  }
+  const rest: {
+    label: string
+    value: string
+  }[] = []
+  for (const [statusCode, count] of counts.entries()) {
+    rest.push({
+      label: `${statusCode} (${numberFormat.format(count)})`,
+      value: `${statusCode}`,
+    })
+  }
+  rest.sort((a, b) => a.value.localeCompare(b.value))
+  const statusCodeChoices = [
+    {
+      label: `ALL (${numberFormat.format(countAll)})`,
+      value: "",
+    },
+    ...rest,
+  ]
   return (
-    <Table mb={30}>
-      <Table.Thead>
-        <Table.Tr>
-          <Table.Th style={{ textAlign: "right" }}>Count</Table.Th>
-          <Table.Th>URL</Table.Th>
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>
-        {rows.map((row) => {
-          return (
-            <Table.Tr key={row.url}>
-              <Table.Td style={{ textAlign: "right" }}>
-                {numberFormat.format(row.count as number)}
-              </Table.Td>
-              <Table.Td onClick={() => setClickedUrl(row.url as string)}>
-                {row.url}
-              </Table.Td>
-            </Table.Tr>
-          )
-        })}
-      </Table.Tbody>
-    </Table>
+    <div>
+      {rest.length === 1 && !searchStatusCode ? (
+        <Text>
+          They're all status_code
+          <Code>{rest[0].value}</Code>
+        </Text>
+      ) : searchStatusCode ? (
+        <Button
+          onClick={() => {
+            const sp = new URLSearchParams(searchParams)
+            sp.delete("searchStatusCode")
+            setSearchParams(sp)
+          }}
+        >
+          reset
+        </Button>
+      ) : (
+        <Select
+          label="Status code"
+          data={statusCodeChoices}
+          value={searchStatusCode || ""}
+          onChange={(statusCode) => {
+            const sp = new URLSearchParams(searchParams)
+            if (statusCode) {
+              sp.set("searchStatusCode", statusCode)
+            } else {
+              sp.delete("searchStatusCode")
+            }
+            setSearchParams(sp)
+          }}
+        />
+      )}
+      <Table mb={30}>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th style={{ textAlign: "right" }}>Count</Table.Th>
+            <Table.Th>Status</Table.Th>
+            <Table.Th>URL</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {rows.map((row) => {
+            const url = row.url as string
+            const statusCode = row.status_code as number
+            const key = `${url}${statusCode}`
+            const count = row.count as number
+            return (
+              <Table.Tr key={key}>
+                <Table.Td style={{ textAlign: "right" }}>
+                  {numberFormat.format(count)}
+                </Table.Td>
+                <Table.Td>{statusCode}</Table.Td>
+                <Table.Td onClick={() => setClickedUrl(url)}>
+                  {row.url}
+                </Table.Td>
+              </Table.Tr>
+            )
+          })}
+        </Table.Tbody>
+      </Table>
+    </div>
   )
 }
