77661dbb7ec6e5c8d09fd69989bd23aa17dc5d04
Peter Bengtsson <mail@peterbe.com>
2025-07-08 21:34:52 -0400

    user agents (#409)

--- src/components/analytics/charts/interval-options.tsx
+++ src/components/analytics/charts/interval-options.tsx
@@ -3,14 +3,16 @@ import { Box, SegmentedControl, Text } from "@mantine/core"
 export function IntervalOptions({
   value,
   onChange,
+  range = [3, 7, 28, 90],
 }: {
   value: string
   onChange: (value: string) => void
+  range?: number[]
 }) {
-  const days = [3, 7, 28]
-  if (new Date(2024, 9, 0) < new Date()) {
-    days.push(90)
-  }
+  const days = range
+  // if (new Date(2024, 9, 0) < new Date()) {
+  //   days.push(90)
+  // }
   const options = days.map((d) => ({ label: `${d} days`, value: `${d}` }))
   return (
     <Box>
